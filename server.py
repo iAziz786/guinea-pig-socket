569757e3de29560820d0496fc11147240471c590
Mohammad Aziz <mdaziz067@gmail.com>
2019-07-27 17:43:41 +0530

    Fix socket connection error

--- server.py
+++ server.py
@@ -1,10 +1,3 @@
 import socket
 
-# new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print(socket.gethostname())
-# new_socket.bind(('192.168.0.102', 9999))
-
-# while True:
-#     new_socket.listen(5)
-
-print (socket.create_connection(('https://python.org', 80)))
+conn = socket.create_connection(('python.org', 443))
